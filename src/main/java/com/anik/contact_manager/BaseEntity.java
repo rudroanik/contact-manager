2c68ea111a45cf64f5043dc90cf9240332b22ae2
Anik Roy <rudroaiub@gmail.com>
2025-11-05 07:29:22 +0600

    introduce api endpoint

--- src/main/java/com/anik/contact_manager/BaseEntity.java
+++ src/main/java/com/anik/contact_manager/BaseEntity.java
@@ -3,10 +3,7 @@ package com.anik.contact_manager;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
 import lombok.Data;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Data
@@ -14,14 +11,4 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue
     Long id;
-
-    @PrePersist
-    private void prePersist() {
-        if (this instanceof Auditable auditable) {
-            LocalDateTime now = LocalDateTime.now();
-            if (auditable.getCreatedBy() == null) {
-                auditable.setCreatedBy(now);
-            }
-        }
-    }
 }
